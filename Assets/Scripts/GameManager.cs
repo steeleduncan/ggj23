5c9f8a770c494f865524a1aac43967c1ed6fcd25
Duncan Steele <steeleduncan@hotmail.com>
2023-02-04 14:48:31 +0000

    work on the sprite system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,13 @@
 using System;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    
-    [SerializeField] private GameObject[] canvases;
+public class GameManager : MonoBehaviour {
+	public Sprite root1, root2, root3, root4;
 
-    // private void Start()
-    // {
-    //     foreach (var item in canvases)
-    //     {
-    //         item.SetActive(true);
-    //     }
-    // }
+	public UnityEngine.U2D.Animation.SpriteResolver spriteResolver;
+
+	void Start() {
+		print($"sr = {spriteResolver}");
+		//		spriteResolver.
+	}
 }
