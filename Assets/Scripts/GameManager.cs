8de3a16e6bbba5d3930d277faf58d0a0a619f368
data <OPRD1@proton.me>
2023-02-04 11:07:24 +0000

    blep

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,11 @@ public class GameManager : MonoBehaviour
     
     [SerializeField] private GameObject[] canvases;
 
-    private void Start()
-    {
-        foreach (var item in canvases)
-        {
-            item.SetActive(true);
-        }
-    }
+    // private void Start()
+    // {
+    //     foreach (var item in canvases)
+    //     {
+    //         item.SetActive(true);
+    //     }
+    // }
 }
